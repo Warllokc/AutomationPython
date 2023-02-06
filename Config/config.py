afaebd063d4867e554b649e6b3abdb86eccc6b03
“Warllokc” <warllokc@yahoo.com>
2023-02-06 07:04:21 -0600

    clean teh project of unecesary files

--- Config/config.py
+++
@@ -1,8 +0,0 @@
-
-class TestData:
-    CHROME_EXECUTABLE_PATH = '/AutomationPython/Pages/chromedriver'
-
-    BASE_URL = "https://opensource-demo.orangehrmlive.com/"
-    USERNAME = "Admin"
-    PASSWORD =  "admin123"
-
---
+++ Config/test_config.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from random import randint
+
+browser = webdriver.Chrome('/Pages/chromedriver')
+
+URL = 'https://opensource-demo.orangehrmlive.com/'
+logi_icon_css = "#divLogo"
+username_textfield_css = "#txtUsername"
+password_textfield_css = "#txtPassword"
+submit_button_css = "#btnLogin"
+forgot_password_xpath = "//*[contains(text(),'Forgot your` password?')]"
+
+page_title = "OrangeHRM"
+
+username = 'Admin'
+password = 'admin123'
+
+LandingPageDashboard = 'Dashboard'
+EmployeeFullNameText = "Employee Full Name"
+
+newUserFirstName = 'Iulian'
+newUserLastName = 'Bondari'
+
+employeeInformationText = 'Employee Information'
+
+personalDetailsText = 'Personal Details'
+
+employeeID = str(randint(200, 10000))
